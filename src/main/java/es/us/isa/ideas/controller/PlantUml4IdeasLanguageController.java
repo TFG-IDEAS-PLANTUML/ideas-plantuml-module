0291c40681c10ee1a4ab7f0a374769f462373486
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2022-03-09 00:32:57 +0100

    Adding integration with ideas-studio. Dependencies added.
    Creation of file with extension puml done.

--- src/main/java/es/us/isa/ideas/controller/PlantUml4IdeasLanguageController.java
+++ src/main/java/es/us/isa/ideas/controller/PlantUml4IdeasLanguageController.java
@@ -2,18 +2,26 @@ package es.us.isa.ideas.controller;
 
 import es.us.isa.ideas.module.common.AppResponse;
 import es.us.isa.ideas.module.controller.BaseLanguageController;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Controller
 @RequestMapping("/ideas-plantuml-language/language")
 public class PlantUml4IdeasLanguageController extends BaseLanguageController {
 
+    @RequestMapping(value = "/operation/{id}/execute", method = RequestMethod.POST)
+    @ResponseBody
     @Override
     public AppResponse executeOperation(String id, String content, String fileUri, String auxArg0, HttpServletRequest httpServletRequest) {
         return new AppResponse();
     }
 
+    @RequestMapping(value = "/format/{format}/checkLanguage", method = RequestMethod.POST)
+    @ResponseBody
     @Override
     public AppResponse checkLanguage(String id, String content, String fileUri, HttpServletRequest httpServletRequest) {
 
@@ -33,6 +41,8 @@ public class PlantUml4IdeasLanguageController extends BaseLanguageController {
         return appResponse;
     }
 
+    @RequestMapping(value = "/convert", method = RequestMethod.POST)
+    @ResponseBody
     @Override
     public AppResponse convertFormat(String currentFormat, String desiredFormat, String fileUri, String content, HttpServletRequest httpServletRequest) {
         return new AppResponse();
