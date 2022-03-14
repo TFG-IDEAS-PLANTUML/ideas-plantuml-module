38ef88b767ac039c3cb365922f643894d8a1b75e
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2022-03-14 19:32:02 +0100

    Adding configuration to start working with ace
    Added mode and theme js script and configured to work with environment. Adapt for PlantUml syntax.
    
    Validation of syntax by controller method done.

--- src/main/java/es/us/isa/ideas/controller/PlantUml4IdeasLanguageController.java
+++ src/main/java/es/us/isa/ideas/controller/PlantUml4IdeasLanguageController.java
@@ -2,46 +2,53 @@ package es.us.isa.ideas.controller;
 
 import es.us.isa.ideas.module.common.AppResponse;
 import es.us.isa.ideas.module.controller.BaseLanguageController;
+import es.us.isa.ideas.service.ValidationDiagramService;
+import net.sourceforge.plantuml.syntax.SyntaxResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/ideas-plantuml-language/language")
 public class PlantUml4IdeasLanguageController extends BaseLanguageController {
 
-    @RequestMapping(value = "/operation/{id}/execute", method = RequestMethod.POST)
+    @Autowired
+    private ValidationDiagramService validationDiagramService;
+
+    @PostMapping(value = "/operation/{id}/execute")
     @ResponseBody
     @Override
     public AppResponse executeOperation(String id, String content, String fileUri, String auxArg0, HttpServletRequest httpServletRequest) {
         return new AppResponse();
     }
 
-    @RequestMapping(value = "/format/{format}/checkLanguage", method = RequestMethod.POST)
+    @PostMapping(value = "/format/{format}/checkLanguage")
     @ResponseBody
     @Override
     public AppResponse checkLanguage(String id, String content, String fileUri, HttpServletRequest httpServletRequest) {
 
-        // Validar sintaxis
-
         AppResponse appResponse = new AppResponse();
 
-        boolean problems = false;
+        SyntaxResult syntaxResult = validationDiagramService.validateFromString(content);
+
+        boolean problems = syntaxResult.isError();
 
         appResponse.setFileUri(fileUri);
 
-        if (problems)
+        if (problems) {
             appResponse.setStatus(AppResponse.Status.OK_PROBLEMS);
-        else
+        } else
             appResponse.setStatus(AppResponse.Status.OK);
 
         return appResponse;
     }
 
-    @RequestMapping(value = "/convert", method = RequestMethod.POST)
+    @PostMapping(value = "/convert")
     @ResponseBody
     @Override
     public AppResponse convertFormat(String currentFormat, String desiredFormat, String fileUri, String content, HttpServletRequest httpServletRequest) {
