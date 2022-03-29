c456d7ed1bab307f3dd1f5afcf2776d5e2bd89e5
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2022-03-30 00:38:29 +0200

    Adding CORS configuration

--- src/main/java/es/us/isa/ideas/PlantUml4IdeasModuleApplication.java
+++ src/main/java/es/us/isa/ideas/PlantUml4IdeasModuleApplication.java
@@ -2,7 +2,10 @@ package es.us.isa.ideas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan("es.us.isa.ideas")
@@ -10,5 +13,16 @@ public class PlantUml4IdeasModuleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlantUml4IdeasModuleApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET",
+                        "PUT", "DELETE", "POST");
+            }
+        };
     }
 }
