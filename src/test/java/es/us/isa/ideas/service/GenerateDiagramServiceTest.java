7db0945101c867423ddca2f5caaa1571d2d0a270
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2022-06-14 22:35:48 +0200

    Providing unitary tests and reviewing app logic

---
+++ src/test/java/es/us/isa/ideas/service/GenerateDiagramServiceTest.java
@@ -0,0 +1,54 @@
+package es.us.isa.ideas.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = {GenerateDiagramService.class})
+public class GenerateDiagramServiceTest {
+
+    @Autowired
+    private GenerateDiagramService generateDiagramService;
+
+    @Test
+    public void generateDiagramFromStringSuccess() {
+
+        String source = "@startuml\n";
+        source += "class Person {\n";
+        source += "String name\n";
+        source += "int age\n";
+        source += "int money\n";
+        source += "String getName()\n";
+        source += "void setName(String name)\n";
+        source += "}\n";
+        source += "@enduml\n";
+
+        Assert.assertTrue("Result returned not expected", this.generateDiagramService.generateDiagramFromString(source).size() > 0);
+
+    }
+
+
+    @Test
+    public void generateDiagramFromStringFail() {
+
+        String source = "@startuml\n";
+        source += "String name\n";
+        source += "int age\n";
+        source += "int money\n";
+        source += "String getName()\n";
+        source += "void setName(String name)\n";
+        source += "}\n";
+        source += "@enduml\n";
+
+        Assert.assertNotNull("Result returned not expected", this.generateDiagramService.generateDiagramFromString(source));
+
+    }
+
+
+}
