d9c4dc843ad144f21c040e3648661d5a89041eaf
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2022-06-15 11:37:31 +0200

    Increasing coverage in project and removing code smells

---
+++ src/test/java/es/us/isa/ideas/controller/TemplatesControllerTest.java
@@ -0,0 +1,48 @@
+package es.us.isa.ideas.controller;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = TemplatesController.class)
+@ActiveProfiles("junit")
+public class TemplatesControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    private static final String BASE_PATH = "/ideas-plantuml-language/template";
+
+    @Test
+    public void getTemplateDocuments() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_PATH + "/document"))
+                .andExpect(status().isOk()).andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+
+    }
+
+    @Test
+    public void testGetTemplateDocuments() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_PATH + "/document/object.puml"))
+                .andExpect(status().isOk()).andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void getTemplateProjects() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get(BASE_PATH + "/project"))
+                .andExpect(status().isOk()).andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+}
