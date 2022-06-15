1e7a02d1bb17641be2d348042251a741505bb336
Angel Delgado Luna <angeldelgadoluna@gmail.com>
2022-06-15 02:40:51 +0200

    Removing useless imports

--- src/main/java/es/us/isa/ideas/configuration/ServiceConfig.java
+++ src/main/java/es/us/isa/ideas/configuration/ServiceConfig.java
@@ -1,7 +1,5 @@
 package es.us.isa.ideas.configuration;
 
-import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -10,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
